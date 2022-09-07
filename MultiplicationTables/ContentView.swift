84fcfc1be0274ef2dd80df298c7498b121d55222
appsaf2k <appsaf2k@gmail.com>
2022-09-07 13:00:26 +0300

    Initial Commit

---
+++ MultiplicationTables/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MultiplicationTables
+//
+//  Created by @andreev2k on 07.09.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
